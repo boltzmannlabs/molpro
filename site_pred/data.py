f3560d7c9348bc76600a9b916caf712d29ac8dc5
Roshanmsb <40595660+Roshanmsb@users.noreply.github.com>
2022-02-07 16:28:48 +0530

    Update data.py

--- site_pred/data.py
+++ site_pred/data.py
@@ -5,8 +5,7 @@ from scipy import ndimage
 import numpy as np
 import torch
 import h5py
-from boltpro.utils.preprocess import rotate_grid, make_3dgrid, Featurizer
-from math import pi
+from molpro.utils.preprocess import make_3dgrid, Featurizer, rotate_grid
 from torch.utils.data import Dataset, DataLoader
 from pytorch_lightning import LightningDataModule
 import os
@@ -32,17 +31,21 @@ def prepare_dataset(data_path: str, hdf_path: str) -> None:
     with h5py.File(hdf_path, mode='w') as f:
         for structure_id in tqdm(ids):
             try:
-                protein_featurizer = Featurizer(os.path.join(data_path, structure_id, 'protein.mol2'), 'mol2', False,
-                                                False, True, True)
-                pocket_featurizer = Featurizer(os.path.join(data_path, structure_id, 'cavity6.mol2'), 'mol2', False,
-                                               False, False, False)
+                protein_featurizer = Featurizer(os.path.join(data_path, structure_id, 'protein.mol2'), 'mol2',
+                                                named_props=['hyb', 'heavydegree', 'heterodegree', 'partialcharge'],
+                                                smarts_labels=['hydrophobic', 'aromatic', 'acceptor', 'donor', 'ring'],
+                                                metal_halogen_encode=True)
+                pocket_featurizer = Featurizer(os.path.join(data_path, structure_id, 'cavity6.mol2'), 'mol2',
+                                               named_props=['hyb', 'heavydegree', 'heterodegree', 'partialcharge'],
+                                               smarts_labels=['hydrophobic', 'aromatic', 'acceptor', 'donor', 'ring'],
+                                               metal_halogen_encode=True)
 
-                pocket_coords = pocket_featurizer.get_coords()
+                pocket_coords = pocket_featurizer.coords
                 pocket_features = np.ones((len(pocket_coords), 1))
-                prot_coords = protein_featurizer.get_coords()
-                prot_features = protein_featurizer.atom_features()
+                prot_coords = protein_featurizer.coords
+                prot_features = protein_featurizer.features
             except:
-                print('Rdkit could not parse file skipping %s' % structure_id)
+                print('openbabel could not parse file skipping %s' % structure_id)
                 continue
 
             centroid = prot_coords.mean(axis=0)
@@ -73,7 +76,7 @@ def prepare_dataset(data_path: str, hdf_path: str) -> None:
                     group.create_dataset(key, data=data, shape=data.shape, dtype='float32', compression='lzf')
 
 
-class SitepredDataset(Dataset):
+class SitePredDataset(Dataset):
     def __init__(self, hdf_path: str, max_dist: int, grid_resolution: int, id_file_path: str, augment: bool) -> None:
         """Pytorch dataset class for preparing 3d grid and labels
             Parameters
@@ -112,34 +115,26 @@ class SitepredDataset(Dataset):
             idx = idx.tolist()
         pdb_id = self.ids[idx]
         if self.transform:
-            theta = choice(range(1, 4)) * pi / 2.0
-            a1 = choice(range(3))
-            axis = np.zeros(3)
-            axis[a1] = 1
+            rot = choice(range(24))
             tr = 5 * np.random.rand(1, 3)
         else:
-            theta = 0
-            axis = np.zeros(3)
+            rot = 0
             tr = (0, 0, 0)
-        rec_grid, pocket_dens = self.prepare_complex(pdb_id, theta, axis, (0, 0, 0), tr)
+        rec_grid, pocket_dens = self.prepare_complex(pdb_id, rotation=rot, translation=tr)
         return rec_grid, pocket_dens
 
-    def prepare_complex(self, pdb_id: str, theta: int, axis: np.ndarray, center: Tuple[int, int, int],
-                        translation: Tuple[int, int, int], v_min: int = 0,
+    def prepare_complex(self, pdb_id: str, rotation: int = 0,
+                        translation: Tuple[int, int, int] = (0, 0, 0), v_min: int = 0,
                         v_max: int = 1) -> Tuple[np.ndarray, np.ndarray]:
         """Transform coordinates and features to 3d probability density grid
             Parameters
             ----------
             pdb_id: str,
                 PDB ID of complex to transform
-            center: tuple, optional
-                center to rotate the 3d grid
-            axis: np.ndarray,
-                axis to rotate the 3d grid
-            theta: int,
-                angle to rotate the grid
             translation: tuple,
                 distance for translation of 3d grid
+            rotation: int,
+                rotation integer that corresponds to certain axis and theta
             v_min: int,
                 minimum density in the grid
             v_max: int,
@@ -150,9 +145,9 @@ class SitepredDataset(Dataset):
         poc_coords = self.data_handle[pdb_id]['pocket_coords'][:]
         poc_features = self.data_handle[pdb_id]['pocket_features'][:]
         prot_features = self.data_handle[pdb_id]['features'][:]
-        prot_coords = rotate_grid(prot_coords, center, axis, theta)
+        prot_coords = rotate_grid(prot_coords, rotation)
         prot_coords += translation
-        poc_coords = rotate_grid(poc_coords, center, axis, theta)
+        poc_coords = rotate_grid(poc_coords, rotation)
         poc_coords += translation
         footprint = ellipsoid(2, 2, 2)
         footprint = footprint.reshape((1, *footprint.shape, 1))
@@ -169,17 +164,15 @@ class SitepredDataset(Dataset):
                                              zoom)
                                 for i in range(poc_features.shape[1])], -1)
         rec_grid = np.squeeze(rec_grid)
-        rec_grid = rec_grid.transpose(3, 0, 1, 2)
-        pocket_dens = pocket_dens.transpose(3, 0, 1, 2)
+        rec_grid = rec_grid.transpose((3, 0, 1, 2))
+        pocket_dens = pocket_dens.transpose((3, 0, 1, 2))
         return rec_grid, pocket_dens
 
 
-class SitepredDataModule(LightningDataModule):
-
+class SitePredDataModule(LightningDataModule):
     def __init__(self, hdf_path: str, max_dist: int, grid_resolution: int, train_ids_path: str, valid_ids_path: str,
                  test_ids_path: str, augment: bool, batch_size: int, num_workers: int, pin_memory: bool):
         super().__init__()
-
         """Pytorch lightning datamodule for preparing train, validation and test dataloader
             Parameters
             ----------
@@ -217,17 +210,20 @@ class SitepredDataModule(LightningDataModule):
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.batch_size = batch_size
+        self.test_dataset = None
+        self.valid_dataset = None
+        self.train_dataset = None
 
     def prepare_data(self, *args, **kwargs):
         pass
 
     def setup(self, **kwargs):
         """define train, test and validation datasets """
-        self.train_dataset = SitepredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
+        self.train_dataset = SitePredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
                                              self.train_ids_path, self.transform)
-        self.valid_dataset = SitepredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
+        self.valid_dataset = SitePredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
                                              self.valid_ids_path, False)
-        self.test_dataset = SitepredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
+        self.test_dataset = SitePredDataset(self.hdf_path, self.max_dist, self.grid_resolution,
                                             self.test_ids_path, False)
 
     def train_dataloader(self):
@@ -254,8 +250,8 @@ def parser_args():
     parser.add_argument("--data_path", type=str, default="data", help="path where mol2 files are stored")
     parser.add_argument("--hdf_path", type=str, default="data.h5",
                         help="path where dataset is stored")
-    hparams = parser.parse_args()
-    return hparams
+    params = parser.parse_args()
+    return params
 
 
 if __name__ == '__main__':
