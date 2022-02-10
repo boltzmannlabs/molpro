2b48bbb2527c3b66789598daa73c092a85b69c24
Roshanmsb <40595660+Roshanmsb@users.noreply.github.com>
2022-02-10 20:27:02 +0530

    Update model.py

--- site_based_gen/model.py
+++ site_based_gen/model.py
@@ -2,44 +2,58 @@ import torch
 from torch import nn
 from pytorch_lightning import Trainer, LightningModule
 from molpro.models.bicycle_gan import Generator, Discriminator, Encoder
-from data import BicycleGANDataModule
+from molpro.site_based_gen.data import SiteGenDataModule
+from molpro.models.unet import compute_per_channel_dice
 from argparse import ArgumentParser
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-pl.seed_everything(1234)
+pl.seed_everything(123)
 
-class BicycleGAN(LightningModule):
-    def __init__(self, z_dim=8, lambda_kl=0.01, lambda_grid=10, lambda_z=0.5, lr=1e-3,
-                 batch_size=2, **kwargs):
+
+class SiteGenModel(LightningModule):
+    def __init__(self, z_dim: int, protein_channels: int, ligand_channels: int, lambda_kl: float,
+                 lambda_grid: float, lambda_z: float, learning_rate: float, **kwargs):
         super().__init__()
         self.save_hyperparameters()
-        self.G = Generator()
-        self.D_cVAE = Discriminator()
-        self.D_cLR = Discriminator()
-        self.E = Encoder()
+        self.G = Generator(z_dim, protein_channels)
+        self.D_cVAE = Discriminator(ligand_channels, protein_channels)
+        self.D_cLR = Discriminator(ligand_channels, protein_channels)
+        self.E = Encoder(z_dim, ligand_channels)
         self.z_dim = z_dim
         self.lambda_kl = lambda_kl
         self.lambda_grid = lambda_grid
         self.lambda_z = lambda_z
-        self.lr = lr
-        self.batch_size = batch_size
+        self.lr = learning_rate
         self.L1_loss = nn.L1Loss()
+        self.criterion = nn.MSELoss()
         self.automatic_optimization = False
 
-    def mse_loss(self,score, target=1):
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser_model = parent_parser.add_argument_group("SiteGenModel")
+        parser_model.add_argument("--z_dim", type=int, default=8)
+        parser_model.add_argument("--protein_channels", type=int, default=7)
+        parser_model.add_argument("--ligand_channels", type=int, default=5)
+        parser_model.add_argument("--lambda_kl", type=int, default=0.01)
+        parser_model.add_argument("--lambda_grid", type=int, default=10)
+        parser_model.add_argument("--lambda_z", type=int, default=0.5)
+        parser_model.add_argument("--learning_rate", type=float, default=1e-4)
+        return parent_parser
+
+    def mse_loss(self, score, target=1):
         if target == 1:
-            labels = torch.ones(score.size(),device=self.device)
+            labels = torch.ones(score.size(), device=self.device)
         elif target == 0:
-            labels = torch.zeros(score.size(),device=self.device)
-        criterion = nn.MSELoss()
-        loss = criterion(score, labels)
+            labels = torch.zeros(score.size(), device=self.device)
+        loss = self.criterion(score, labels)
         return loss
 
-    def forward(self, x, z) :
+    def forward(self, x, z):
         return self.G(x, z)
 
     def get_z_random(self, batch_size):
-        z = torch.randn(batch_size, self.z_dim, device = self.device)
+        z = torch.randn(batch_size, self.z_dim, device=self.device)
         return z.detach()
 
     def encode(self, input_grid):
@@ -57,6 +71,13 @@ class BicycleGAN(LightningModule):
             if net is not None:
                 for param in net.parameters():
                     param.requires_grad = requires_grad
+
+    def configure_optimizers(self):
+        g_opt = torch.optim.Adam(self.G.parameters(), lr=self.lr)
+        e_opt = torch.optim.Adam(self.E.parameters(), lr=self.lr)
+        d_cVAE_opt = torch.optim.Adam(self.D_cVAE.parameters(), lr=self.lr)
+        d_cLR_opt = torch.optim.Adam(self.D_cLR.parameters(), lr=self.lr)
+        return g_opt, e_opt, d_cVAE_opt, d_cLR_opt
 
     def training_step(self, batch, batch_idx):
 
@@ -107,11 +128,10 @@ class BicycleGAN(LightningModule):
         grid_recon_loss = self.lambda_grid * self.L1_loss(fake_B_encoded, real_B_encoded)
 
         G_loss = G_GAN_loss + KL_div + grid_recon_loss
-        
-        G_alone_loss = self.lambda_z * self.L1_loss(mu2, z_random)
-        
-        if torch.isnan(G_loss) == False:
 
+        G_alone_loss = self.lambda_z * self.L1_loss(mu2, z_random)
+
+        if not torch.isnan(G_loss):
             self.manual_backward(G_loss, retain_graph=True)
 
             self.set_requires_grad([self.E], False)
@@ -136,9 +156,8 @@ class BicycleGAN(LightningModule):
         D_loss_cVAE_2 = self.mse_loss(real_d_cVAE_2, 1) + self.mse_loss(fake_d_cVAE_2, 0)
 
         D_loss_cVAE = D_loss_cVAE_1 + D_loss_cVAE_2
-        
-        
-        if torch.isnan(D_loss_cVAE) == False:
+
+        if not torch.isnan(D_loss_cVAE):
             self.manual_backward(D_loss_cVAE)
             d_cVAE_opt.step()
 
@@ -150,44 +169,52 @@ class BicycleGAN(LightningModule):
         D_loss_cLR_2 = self.mse_loss(real_d_cLR_2, 1) + self.mse_loss(fake_d_cLR_2, 0)
 
         D_loss_cLR = D_loss_cLR_1 + D_loss_cLR_2
-        
-        if torch.isnan(D_loss_cLR) == False:
+
+        if not torch.isnan(D_loss_cLR):
             self.manual_backward(D_loss_cLR)
             d_cLR_opt.step()
 
-        self.log_dict({'g_loss': G_alone_loss, 'd_loss_cVAE': D_loss_cVAE, 'd_loss_cLR': D_loss_cLR, 'g_e_loss' : G_loss}, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log_dict({'g_loss': G_alone_loss, 'd_loss_cVAE': D_loss_cVAE, 'd_loss_cLR': D_loss_cLR,
+                       'g_e_loss': G_loss}, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
+    def validation_step(self, batch, batch_idx):
+        protein_grid, ligand_grid = batch
+        z_random = self.get_z_random(batch.shape[0])
+        preds = self.G(protein_grid, z_random)
+        dice = torch.mean(compute_per_channel_dice(preds, ligand_grid, 1e-6))
+        self.log('val_dice', dice, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
-    def configure_optimizers(self):
-        g_opt = torch.optim.Adam(self.G.parameters(), lr=self.lr)
-        e_opt = torch.optim.Adam(self.E.parameters(), lr=self.lr)
-        d_cVAE_opt = torch.optim.Adam(self.D_cVAE.parameters(), lr=self.lr)
-        d_cLR_opt = torch.optim.Adam(self.D_cLR.parameters(), lr=self.lr)
-        return g_opt, e_opt, d_cVAE_opt, d_cLR_opt
-
-
-def parser_args():
-    parser = ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=100, help="epochs")
-    parser.add_argument("--max_dist", type=int, default=23, help="maximum distance for atoms from the center of grid")
-    parser.add_argument("--grid_resolution", type=int, default=2, help="resolution for the grid")
-    parser.add_argument("--augment", type=bool, default=False, help="perform augmentation of dataset")
-    parser.add_argument("--hdf_path", type=str, default="train.hdf",
-                        help="path where dataset is stored")
-    parser.add_argument("--batch_size", type=int, default=2, help="batch size for pytorch dataloader")
-    parser.add_argument("--num_workers", type=int, default=8, help="number of workers for pytorch dataloader")
-    parser.add_argument("--pin_memory", type=bool, default=True, help="whether to pin memory for pytorch dataloader")
-    parser.add_argument("--gpus", type=int, default=1, help="number of gpus to be used for training")
-    hparams = parser.parse_args()
-    return hparams
+    def test_step(self, batch, batch_idx):
+        protein_grid, ligand_grid = batch
+        z_random = self.get_z_random(batch.shape[0])
+        preds = self.G(protein_grid, z_random)
+        dice = torch.mean(compute_per_channel_dice(preds, ligand_grid, 1e-6))
+        self.log('test_dice', dice, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
 
 if __name__ == '__main__':
-    hparams = parser_args()
-    model = BicycleGAN()
+    parser = ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=2, help="batch size. Default cannot be changed")
+    parser.add_argument("--max_dist", type=int, default=23, help="maximum distance for atoms from the center of grid")
+    parser.add_argument("--grid_resolution", type=int, default=2, help="resolution for the grid")
+    parser.add_argument("--augment", type=bool, default=True, help="perform augmentation of dataset")
+    parser.add_argument("--hdf_path", type=str, default="data.h5",
+                        help="path where dataset is stored")
+    parser.add_argument("--train_ids_path", type=str, default="sample_data/site_based_gen/splits/train.txt",
+                        help="path where list of train ids is stored")
+    parser.add_argument("--val_ids_path", type=str, default="sample_data/site_based_gen/splits/valid.txt",
+                        help="path where list of validation ids is stored")
+    parser.add_argument("--test_ids_path", type=str, default="sample_data/site_based_gen/splits/test.txt",
+                        help="path where list of test ids is stored")
+    parser.add_argument("--num_workers", type=int, default=8, help="number of workers for pytorch dataloader")
+    parser.add_argument("--pin_memory", type=bool, default=True, help="whether to pin memory for pytorch dataloader")
+    parser = SiteGenModel.add_model_specific_args(parser)
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+    model = SiteGenModel(**vars(args))
     print(model.hparams)
-    data_module = BicycleGANDataModule(hparams.hdf_path, hparams.max_dist, hparams.grid_resolution,
-                                       'train.txt', hparams.augment, hparams.batch_size,
-                                       hparams.num_workers, hparams.pin_memory)
-    trainer = Trainer(gpus=hparams.gpus, max_epochs=hparams.epochs, terminate_on_nan=True)
+    data_module = SiteGenDataModule(args.hdf_path, args.max_dist, args.grid_resolution,
+                                    args.train_ids_path, args.val_ids_path, args.test_ids_path, args.augment,
+                                    args.batch_size, args.num_workers, args.pin_memory)
+    trainer = Trainer.from_argparse_args(args, callbacks=[ModelCheckpoint(monitor='val_dice', mode='max')])
     trainer.fit(model, data_module)
