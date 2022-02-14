0cc0dea1d4d6a3ed472654cea4362488dcf79c00
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-02-14 16:18:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ pip install torch-scatter -f https://data.pyg.org/whl/torch-1.8.1+cpu
 pip install torch-sparse -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
 pip install torch-cluster -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
 pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
-pip install torch-geometric == 1.7.2
+pip install torch-geometric==1.7.2
 pip install pytorch-lightning
 pip install omegaconf
 pip install pot
