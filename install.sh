a8967211fb334d4e5fbb9c6640c40b2eb8122563
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-02-14 14:32:42 +0530

    Update and rename requirements.txt to install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+pip install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.8.1+cpu
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
+pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
+pip install torch-geometric == 1.7.2
+pip install pytorch-lightning
+pip install omegaconf
+pip install pot
+pip install rdkit-pypi
