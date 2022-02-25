c5e7a06f7f20b94e37d75e1b069129ef6da835ac
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-02-25 11:22:42 +0530

    Update predict.py

--- molpro/shape_based_gen/predict.py
+++ molpro/shape_based_gen/predict.py
@@ -7,7 +7,8 @@ from typing import Callable, List
 from model import ShapeBasedGenModule
 from data import vocab_i2c_v1,vocab_c2i_v1
 from molpro.models.shape_captioning import ShapeEncoder, DecoderRNN, VAE
-import molpro.utils.preprocess as featurization
+from molpro.utils.preprocess import make_3dgrid, Featurizer, rotate_grid
+from random import choice
 
 
 def unique_canonical(smiles_list: List[str] = None) -> List[str]:
@@ -45,9 +46,9 @@ def initialize_model(ckpt_path: str = None,device:str = "cpu"):
         model : loaded model in a object
 
     """
-    encoder = ShapeEncoder(9)
+    encoder = ShapeEncoder(5)
     decoder = DecoderRNN(512, 1024, 29, 1,device)
-    vae_model = VAE(nc=9,device=device)
+    vae_model = VAE(nc=5,device=device)
     model = ShapeBasedGenModule(encoder, decoder, vae_model)
     ckpt = torch.load(ckpt_path)
     model.load_state_dict(ckpt['state_dict'])
@@ -70,16 +71,33 @@ def featurize_smile(smile:str = None) -> torch.tensor:
     vox : torch.tensor,
              a tensor which contains featurization(shape) of that smile
     """
-    featurizer = featurization.Featurizer(smile)
-    featurizer.generate_conformer()
-    featurizer.get_coords
+    fatures_list = ['hydrophobic', 'aromatic','acceptor', 'donor','ring']
+
+    featurizer = Featurizer(input_file = smile , file_type= 'smi', named_props  = ["partialcharge"], smarts_labels = fatures_list, metal_halogen_encode = False)
+
+
     coords = featurizer.coords
     centroid = coords.mean(axis=0)
     coords -= centroid
-    featurizer.atom_features
     afeats = featurizer.features
-    vox = featurization.make_3dgrid(coords, afeats, 23, 2)
-    vox = torch.tensor(np.squeeze(vox, 0).transpose(3, 0, 1, 2)).unsqueeze(0)
+    features1 = afeats[:,:5]
+    features2 = afeats[:,3:]
+    rot = choice(range(24))
+    tr1 = 2 * np.random.rand(1, 3)
+    tr2 = 0.5 * np.random.rand(1, 3)
+    coords1 = rotate_grid(coords,rot)
+    coords1 += tr1
+    f1n = make_3dgrid(coords1,features1,23,2)
+
+    coords2 = rotate_grid(coords,rot)
+    coords2 += tr2
+    f2n = make_3dgrid(coords2,features2,23,2)
+
+    feats_final = np.concatenate([f1n,f2n],axis=4)
+
+
+    vox = np.squeeze(feats_final, 0).transpose(3, 0, 1, 2)
+    vox = vox.unsqueeze(0)
 
     return vox
 
@@ -101,12 +119,12 @@ def decode_smiles(model_outputs:torch.tensor = None) -> List[str]:
 
     """
 
+    model_outputs = torch.stack(model_outputs,1)
     decoded_smiles = []
-    for op in model_outputs:
-        op = op[0]
+    for tensor in model_outputs:
         smile = str()
-        for i in op[1:]:
-            char  = vocab_i2c_v1.get(int(i[0]))
+        for i in tensor[1:]:
+            char  = vocab_i2c_v1.get(int(i))
             if char == "end":
                 break
             smile = smile+str(char)
@@ -147,18 +165,20 @@ def generate_smiles(input_smiles :List[str]= None, ckpt_path :str = None,
                             "smile_+str(len(input_smiles))": [gen_smi_1,gen_smi_2.......,gen_smi_n] }
         """
 
+    """if not input_smiles or ckpt_path is None:
+        raise TypeError('Please give right input_molecule and ckpt file.')"""
 
     model  = initialize_model(ckpt_path = ckpt_path,device = device)
     
     generated_smiles = dict()
     for smi in input_smiles:
-        vox = featurize_smile(smi)
-        model_outputs = [[model.prediction(vox,sample_prob=sample_prob)] for i in range(n_attempts)]
+        vox = featurize_smile(smi).repeat(n_attempts,1,1,1,1)
+        model_outputs = model.prediction((vox,None,None),sample_prob=sample_prob,factor=1.)
         decoded_smiles = decode_smiles(model_outputs = model_outputs)
         if unique_valid :
-            generated_smiles["smi"] = unique_canonical(decoded_smiles)
+            generated_smiles[smi] = unique_canonical(decoded_smiles)
         else :
-            generated_smiles["smi"] = decoded_smiles
+            generated_smiles[smi] = decoded_smiles
 
     return generated_smiles
 
