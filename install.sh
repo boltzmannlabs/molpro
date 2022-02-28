d54359d2346da0f8662ccbd7eea537d0531baf54
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-02-28 13:56:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-pip install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.8.1+cpu
-pip install torch-sparse -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
-pip install torch-cluster -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
-pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.8.1+cpu.html
+pip install torch==1.8.1+{cuda_version_OR_cpu} torchvision==0.9.1+{cuda_version_OR_cpu} torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.8.1+{cuda_version_OR_cpu}
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.8.1+{cuda_version_OR_cpu}.html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-1.8.1+{cuda_version_OR_cpu}.html
+pip install torch-spline-conv -f https://data.pyg.org/whl/torch-1.8.1+{cuda_version_OR_cpu}.html
 pip install torch-geometric==1.7.2
 pip install pytorch-lightning
-pip install omegaconf
 pip install pot
 pip install rdkit-pypi
+conda install -c conda-forge openbabel
+pip install h5py
+pip install scikit-image
