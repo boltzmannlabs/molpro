e6dacb9c85f99e3a9c82f3f16a32f687667380c4
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-03-02 10:31:34 +0530

    Update shape_based_gen_tests.py

--- molpro/tests/shape_based_gen_tests.py
+++ molpro/tests/shape_based_gen_tests.py
@@ -3,14 +3,14 @@ from models.shape_captioning import ShapeEncoder, DecoderRNN, VAE
 from model import BpModule
 
 
-encoder = ShapeEncoder(9)
+encoder = ShapeEncoder(5)
 decoder = DecoderRNN(512, 1024, 29, 1,"cpu")
-vae_model = VAE(nc=9,device="cpu")
+vae_model = VAE(nc=5,device="cpu")
 
 model = BpModule(encoder, decoder, vae_model)
 
 
-mol_batch = torch.rand((1, 9, 24, 24, 24))
+mol_batch = torch.rand((1, 8, 24, 24, 24))
 caption = torch.rand([1, 34])
 lengths = (34,)
 x = (mol_batch, caption, lengths)
@@ -19,7 +19,7 @@ def test_shape_gen():
     recon_batch,mu,logvar = model(x,only_vae=True)
     condition1 = recon_batch is not None and mu is not None and logvar is not None
     condition2 = type(recon_batch).__name__ == 'Tensor' and type(mu).__name__ == 'Tensor' and type(logvar).__name__ == 'Tensor'
-    condition3 = recon_batch.size() == (1, 9, 24, 24, 24) and mu.size() == (1,512) and logvar.size() == (1,512)
+    condition3 = recon_batch.size() == (1, 8, 24, 24, 24) and mu.size() == (1,512) and logvar.size() == (1,512)
     assert condition1 and condition2 and condition3
 
 
