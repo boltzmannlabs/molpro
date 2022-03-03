1c2db39e812b659634fed51fae7c4077a2578575
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-03-03 11:45:51 +0530

    Update model.py

--- molpro/geomol/model.py
+++ molpro/geomol/model.py
@@ -18,7 +18,6 @@ def add_train_args(parser: ArgumentParser):
     """
     # General arguments
     parser.add_argument('--data_dir', type=str)
-    parser.add_argument('--split_path', type=str)
     parser.add_argument('--dataset', type=str, default='drugs')
     parser.add_argument('--seed', type=int, default=0)
 
@@ -30,29 +29,6 @@ def add_train_args(parser: ArgumentParser):
     parser.add_argument('--num_workers', type=int, default=2)
 
     parser.add_argument('--verbose', action='store_true', default=False)
-
-    # Model arguments
-    parser.add_argument('--model_dim', type=int, default=25)
-    parser.add_argument('--random_vec_dim', type=int, default=10)
-    parser.add_argument('--random_vec_std', type=float, default=1)
-    parser.add_argument('--random_alpha', action='store_true', default=False)
-    parser.add_argument('--n_true_confs', type=int, default=10)
-    parser.add_argument('--n_model_confs', type=int, default=10)
-
-    parser.add_argument('--gnn1_depth', type=int, default=3)
-    parser.add_argument('--gnn1_n_layers', type=int, default=2)
-    parser.add_argument('--gnn2_depth', type=int, default=3)
-    parser.add_argument('--gnn2_n_layers', type=int, default=2)
-    parser.add_argument('--encoder_n_head', type=int, default=2)
-    parser.add_argument('--coord_pred_n_layers', type=int, default=2)
-    parser.add_argument('--d_mlp_n_layers', type=int, default=1)
-    parser.add_argument('--h_mol_mlp_n_layers', type=int, default=1)
-    parser.add_argument('--alpha_mlp_n_layers', type=int, default=2)
-    parser.add_argument('--c_mlp_n_layers', type=int, default=1)
-
-    parser.add_argument('--global_transformer', action='store_true', default=False)
-    parser.add_argument('--loss_type', type=str, default='ot_emd')
-    parser.add_argument('--teacher_force', action='store_true', default=False)
 
 def parse_train_args() -> Namespace:
     """
@@ -69,31 +45,28 @@ def parse_train_args() -> Namespace:
 
 def set_hyperparams(args):
     """
-    Converts ArgumentParser args to hyperparam dictionary.
-
-    :param args: Namespace containing the args.
-    :return: A dictionary containing the args as hyperparams.
+    return: A dictionary containing hyperparams.
     """
 
-    hyperparams = {'model_dim': args.model_dim,
-                   'random_vec_dim': args.random_vec_dim,
-                   'random_vec_std': args.random_vec_std,
-                   'global_transformer': args.global_transformer,
-                   'n_true_confs': args.n_true_confs,
-                   'n_model_confs': args.n_model_confs,
-                   'gnn1': {'depth': args.gnn1_depth,
-                            'n_layers': args.gnn1_n_layers},
-                   'gnn2': {'depth': args.gnn2_depth,
-                            'n_layers': args.gnn2_n_layers},
-                   'encoder': {'n_head': args.encoder_n_head},
-                   'coord_pred': {'n_layers': args.coord_pred_n_layers},
-                   'd_mlp': {'n_layers': args.d_mlp_n_layers},
-                   'h_mol_mlp': {'n_layers': args.h_mol_mlp_n_layers},
-                   'alpha_mlp': {'n_layers': args.alpha_mlp_n_layers},
-                   'c_mlp': {'n_layers': args.c_mlp_n_layers},
-                   'loss_type': args.loss_type,
-                   'teacher_force': args.teacher_force,
-                   'random_alpha': args.random_alpha}
+    hyperparams = {'model_dim': 25,
+                   'random_vec_dim': 10,
+                   'random_vec_std': 1,
+                   'global_transformer': False,
+                   'n_true_confs': 10,
+                   'n_model_confs': 10,
+                   'gnn1': {'depth': 3,
+                            'n_layers': 2},
+                   'gnn2': {'depth': 3,
+                            'n_layers': 2},
+                   'encoder': {'n_head': 2},
+                   'coord_pred': {'n_layers': 2},
+                   'd_mlp': {'n_layers': 1},
+                   'h_mol_mlp': {'n_layers': 1},
+                   'alpha_mlp': {'n_layers': 2},
+                   'c_mlp': {'n_layers': 1},
+                   'loss_type': "ot_emd",
+                   'teacher_force': False,
+                   'random_alpha': False}
 
     return hyperparams
 
