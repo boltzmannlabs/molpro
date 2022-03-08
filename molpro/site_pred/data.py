ed1e4b2067ba97710630182292539ffb611f5c41
Roshanmsb <40595660+Roshanmsb@users.noreply.github.com>
2022-03-08 16:04:53 +0530

    Update data.py

--- molpro/site_pred/data.py
+++ molpro/site_pred/data.py
@@ -1,16 +1,12 @@
-from random import choice
-from skimage.draw import ellipsoid
-from scipy import ndimage
 import numpy as np
-import torch
 import h5py
-from molpro.utils.preprocess import make_3dgrid, Featurizer, rotate_grid
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
 import os
-from typing import Tuple
 from tqdm.auto import tqdm
 from argparse import ArgumentParser
+from molpro.utils.preprocess import make_3dgrid, Featurizer, rotate_grid
+from molpro.utils.dataset import SitePredDataset
 
 
 def prepare_dataset(data_path: str, hdf_path: str) -> None:
@@ -73,99 +69,6 @@ def prepare_dataset(data_path: str, hdf_path: str) -> None:
                                   ('pocket_features', pocket_features),
                                   ('centroid', centroid)):
                     group.create_dataset(key, data=data, shape=data.shape, dtype='float32', compression='lzf')
-
-
-class SitePredDataset(Dataset):
-    def __init__(self, hdf_path: str, max_dist: int, grid_resolution: int, id_file_path: str, augment: bool) -> None:
-        """Pytorch dataset class for preparing 3d grid and labels
-            Parameters
-            ----------
-            hdf_path: str,
-                Path to save the HDF5 file
-            grid_resolution: float
-                Resolution of a grid (in Angstroms)
-            max_dist: float
-                Maximum distance between atom and box center. Resulting box has size of
-                2*`max_dist`+1 Angstroms and atoms that are too far away are not
-                included.
-            id_file_path: str,
-                Path to text file containing pdb ids
-            augment: bool,
-                Whether to augment the 3d grid or not
-        """
-
-        self.transform = augment
-        self.max_dist = max_dist
-        self.grid_resolution = grid_resolution
-        self.hdf_path = hdf_path
-        self.data_handle = None
-        f = open(id_file_path)
-        ids = f.read().splitlines()
-        f.close()
-        self.ids = ids
-
-    def __len__(self):
-        return len(self.ids)
-
-    def __getitem__(self, idx):
-        if self.data_handle is None:
-            self.data_handle = h5py.File(self.hdf_path, 'r')
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        pdb_id = self.ids[idx]
-        if self.transform:
-            rot = choice(range(24))
-            tr = 5 * np.random.rand(1, 3)
-        else:
-            rot = 0
-            tr = (0, 0, 0)
-        rec_grid, pocket_dens = self.prepare_complex(pdb_id, rotation=rot, translation=tr)
-        return rec_grid, pocket_dens
-
-    def prepare_complex(self, pdb_id: str, rotation: int = 0,
-                        translation: Tuple[int, int, int] = (0, 0, 0), v_min: int = 0,
-                        v_max: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Transform coordinates and features to 3d probability density grid
-            Parameters
-            ----------
-            pdb_id: str,
-                PDB ID of complex to transform
-            translation: tuple,
-                distance for translation of 3d grid
-            rotation: int,
-                rotation integer that corresponds to certain axis and theta
-            v_min: int,
-                minimum density in the grid
-            v_max: int,
-                maximum density in the grid
-        """
-
-        prot_coords = self.data_handle[pdb_id]['coords'][:]
-        poc_coords = self.data_handle[pdb_id]['pocket_coords'][:]
-        poc_features = self.data_handle[pdb_id]['pocket_features'][:]
-        prot_features = self.data_handle[pdb_id]['features'][:]
-        prot_coords = rotate_grid(prot_coords, rotation)
-        prot_coords += translation
-        poc_coords = rotate_grid(poc_coords, rotation)
-        poc_coords += translation
-        footprint = ellipsoid(2, 2, 2)
-        footprint = footprint.reshape((1, *footprint.shape, 1))
-        rec_grid = make_3dgrid(prot_coords, prot_features, max_dist=self.max_dist,
-                               grid_resolution=self.grid_resolution)
-        pocket_dens = make_3dgrid(poc_coords, poc_features, max_dist=self.max_dist,
-                                  grid_resolution=1)
-        margin = ndimage.maximum_filter(pocket_dens, footprint=footprint)
-        pocket_dens += margin
-        pocket_dens = pocket_dens.clip(v_min, v_max)
-
-        zoom = rec_grid.shape[1] / pocket_dens.shape[1]
-        pocket_dens = np.stack([ndimage.zoom(pocket_dens[0, ..., i],
-                                             zoom)
-                                for i in range(poc_features.shape[1])], -1)
-        rec_grid = np.squeeze(rec_grid)
-        rec_grid = rec_grid.transpose((3, 0, 1, 2))
-        pocket_dens = pocket_dens.transpose((3, 0, 1, 2))
-        return rec_grid, pocket_dens
 
 
 class SitePredDataModule(LightningDataModule):
