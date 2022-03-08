283c65e4d8091710a8db80174a8950e1a8c78d06
Roshanmsb <40595660+Roshanmsb@users.noreply.github.com>
2022-03-08 16:03:26 +0530

    Update data.py

--- molpro/affinity_pred/data.py
+++ molpro/affinity_pred/data.py
@@ -1,15 +1,12 @@
-from random import choice
-import numpy as np
-import torch
 import h5py
-from molpro.utils.preprocess import rotate_grid, make_3dgrid, Featurizer
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
 import os
-from typing import Tuple
 from tqdm.auto import tqdm
 from argparse import ArgumentParser
+from molpro.utils.preprocess import rotate_grid, make_3dgrid, Featurizer
+from molpro.utils.dataset import AffinityPredDataset
 
 
 def prepare_dataset(data_path: str, hdf_path: str, df_path: str) -> None:
@@ -58,86 +55,6 @@ def prepare_dataset(data_path: str, hdf_path: str, df_path: str) -> None:
                 group.create_dataset(key, data=data, shape=data.shape, dtype='float32', compression='lzf')
 
 
-class AffinityPredDataset(Dataset):
-
-    def __init__(self, hdf_path: str, max_dist: int, grid_resolution: int, id_file_path: str, augment: bool) -> None:
-        """Pytorch dataset class for preparing 3d grid and labels
-            Parameters
-            ----------
-            hdf_path: str,
-                Path to save the HDF5 file
-            grid_resolution: float
-                Resolution of a grid (in Angstroms)
-            max_dist: float
-                Maximum distance between atom and box center. Resulting box has size of
-                2*`max_dist`+1 Angstroms and atoms that are too far away are not
-                included.
-            id_file_path: str,
-                Path to text file containing pdb ids
-            augment: bool,
-                Whether to augment the 3d grid or not
-        """
-
-        self.transform = augment
-        self.max_dist = max_dist
-        self.grid_resolution = grid_resolution
-        self.hdf_path = hdf_path
-        self.data_handle = None
-        f = open(id_file_path)
-        ids = f.read().splitlines()
-        f.close()
-        self.ids = ids
-
-    def __len__(self):
-        return len(self.ids)
-
-    def __getitem__(self, idx):
-        if self.data_handle is None:
-            self.data_handle = h5py.File(self.hdf_path, 'r')
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        pdb_id = self.ids[idx]
-        pkd = self.data_handle[pdb_id]['pkd'][:]
-        if self.transform:
-            rot = choice(range(24))
-            tr = 5 * np.random.rand(1, 3)
-        else:
-            rot = 0
-            tr = (0, 0, 0)
-        rec_grid, lig_grid = self.prepare_complex(pdb_id, rotation=rot, translation=tr)
-
-        return np.concatenate((rec_grid, lig_grid)), pkd
-
-    def prepare_complex(self, pdb_id: str, rotation: int = 0,
-                        translation: Tuple[int, int, int] = (0, 0, 0)) -> Tuple[np.ndarray, np.ndarray]:
-        """Transform coordinates and features to 3d probability density grid
-            Parameters
-            ----------
-            pdb_id: str,
-                PDB ID of complex to transform
-            translation: tuple,
-                distance for translation of 3d grid
-            rotation: int,
-                rotation integer that corresponds to certain axis and theta
-        """
-
-        prot_coords = self.data_handle[pdb_id]['prot_coords'][:]
-        ligand_coords = self.data_handle[pdb_id]['ligand_coords'][:]
-        ligand_features = self.data_handle[pdb_id]['ligand_features'][:]
-        prot_features = self.data_handle[pdb_id]['prot_features'][:]
-        prot_coords = rotate_grid(prot_coords, rotation)
-        prot_coords += translation
-        ligand_coords = rotate_grid(ligand_coords, rotation)
-        ligand_coords += translation
-        rec_grid = make_3dgrid(prot_coords, prot_features, max_dist=self.max_dist,
-                               grid_resolution=self.grid_resolution)
-        lig_grid = make_3dgrid(ligand_coords, ligand_features, max_dist=self.max_dist,
-                               grid_resolution=self.grid_resolution)
-        rec_grid = rec_grid.squeeze(0).transpose((3, 0, 1, 2))
-        lig_grid = lig_grid.squeeze(0).transpose((3, 0, 1, 2))
-        return rec_grid, lig_grid
-
-        
 class AffinityPredDataModule(LightningDataModule):
 
     def __init__(self, hdf_path: str, max_dist: int, grid_resolution: int, train_ids_path: str, valid_ids_path: str,
