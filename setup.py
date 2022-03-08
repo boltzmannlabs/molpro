502a713415cf136051ee559a12bfff14065e44a5
Utkarsh Parashar <utkarshjhansi11@gmail.com>
2022-03-08 22:54:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup, find_packages
-setup(name = "molpro", version = 0.1, packages = find_packages())
+setup(name = "molpro", version = 0.1,
+      author="Boltzmann Labs",
+      author_email="contact@boltzmann.co",
+      description="A python package for 3d structure based drug design,
+      url="https://github.com/boltzmannlabs/molpro",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License",
+        "Operating System :: OS Independent",],
+      packages = find_packages()
+     )
